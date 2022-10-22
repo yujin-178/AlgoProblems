971dda7d9067ca04a340d833818736ac1bf4723a
김유진 <determination9212@gmail.com>
2022-10-23 00:23:25 +0900

    feat: 201023 사라지는 발판 수정 중...

--- Programmers/src/Level3/P92345_사라지는_발판/Solution2.java
+++ Programmers/src/Level3/P92345_사라지는_발판/Solution2.java
@@ -1,16 +1,6 @@
 package Level3.P92345_사라지는_발판;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-
 public class Solution2 {
-    static int R, C;
-    static int[][] map;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static ArrayList<ResultInfo>[] resultInfo;
-    static Player p1, p2;
-    static PriorityQueue<ResultInfo> pq;
 
     public static void main(String[] args) {
 //        답 4
@@ -30,76 +20,88 @@ public class Solution2 {
         System.out.println(solution(board, aloc, bloc));
     }
 
+    static int R, C;
+    static int[][] map;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
     static public int solution(int[][] board, int[] aloc, int[] bloc) {
         R = board.length;
         C = board[0].length;
-        p1 = new Player(aloc[0], aloc[1]);
-        p2 = new Player(bloc[0], bloc[1]);
+        Player p1 = new Player(aloc[0], aloc[1]);
+        Player p2 = new Player(bloc[0], bloc[1]);
         map = board;
-        pq = new PriorityQueue<>();
-        resultInfo = new ArrayList[2];
-        resultInfo[0] = new ArrayList<>();
-        resultInfo[1] = new ArrayList<>();
+        ResultInfo ans = p1Move(0, p1, p2);
 
-        playerMove(0);
-        System.out.println("A가 승리한 횟수 : " + resultInfo[0].size());
-        System.out.println("B가 승리한 횟수 : " + resultInfo[1].size());
-        int cnt = pq.peek().cnt;
-        while (!pq.isEmpty()) {
-            System.out.println(pq.poll());
-        }
-
-        return cnt;
+        return ans.cnt;
     }
 
-    static void viewTest() {
-        System.out.println();
-        for (int r = 0; r < R; r++) {
-            for (int c = 0; c < C; c++) {
-                if (p1.r == r && p1.c == c) {
-                    System.out.print("A ");
-                } else if (p2.r == r && p2.c == c) {
-                    System.out.print("B ");
-                } else {
-                    System.out.print(map[r][c] + " ");
-                }
-            }
-            System.out.println();
-        }
-    }
+    static ResultInfo p1Move(int dep, Player p1, Player p2) {
 
-    static void playerMove(int dep) {
-        int idx = dep % 2;
-        Player now = idx == 0 ? p1 : p2;
-        int r = now.r;
-        int c = now.c;
+        int maxNum = 0;
+        int minNum = Integer.MAX_VALUE;
+        boolean playerStatus = false;
 
-        if (map[r][c] == 0) {
-            resultInfo[1].add(new ResultInfo(idx, dep));
-            pq.add(new ResultInfo(idx, dep));
-        }
+        // 발판이 없다면 패배 및 0
+        if (map[p1.r][p1.c] == 0)
+            return new ResultInfo(false, 0);
 
-        boolean flag = true;
-        map[r][c] = 0;
+        map[p1.r][p1.c] = 0;
         for (int d = 0; d < 4; d++) {
-            int nr = r + dr[d];
-            int nc = c + dc[d];
+            int nr = p1.r + dr[d];
+            int nc = p1.c + dc[d];
             if (chkOut(nr, nc)) continue;
-            flag = false;
-            p1.r = nr;
-            p1.c = nc;
 
-            playerMove(dep + 1);
+
+            ResultInfo tmp = p2Move(dep + 1, new Player(nr, nc), p2);
+            playerStatus |= !tmp.winner;
+            if (playerStatus) {
+                minNum = Math.min(minNum, tmp.cnt + 1);
+            }
+            else {
+                maxNum = Math.max(maxNum, tmp.cnt + 1);
+            }
+//            System.out.println("p1 : " +tmp);
+
         }
-        map[r][c] = 1;
-        p1.r = r;
-        p1.c = c;
-        if (flag) {
-            resultInfo[1].add(new ResultInfo(idx, dep));
-            pq.add(new ResultInfo(idx, dep));
-        }
+        map[p1.r][p1.c] = 1;
+//        viewTest(p1, p2, minNum, maxNum, dep+1);
+        return new ResultInfo(playerStatus, playerStatus ? minNum : maxNum);
     }
 
+    static ResultInfo p2Move(int dep, Player p1, Player p2) {
+
+        int maxNum = 0;
+        int minNum = Integer.MAX_VALUE;
+        boolean playerStatus = false;
+
+        // 발판이 없다면 패배 및 0
+        if (map[p2.r][p2.c] == 0)
+            return new ResultInfo(false, 0);
+
+        map[p2.r][p2.c] = 0;
+        for (int d = 0; d < 4; d++) {
+            int nr = p2.r + dr[d];
+            int nc = p2.c + dc[d];
+            if (chkOut(nr, nc)) continue;
+
+
+            ResultInfo tmp = p1Move(dep + 1, p1, new Player(nr, nc));
+            playerStatus |= !tmp.winner;
+
+            if (playerStatus) {
+                minNum = Math.min(minNum, tmp.cnt + 1);
+            }
+            else {
+                maxNum = Math.max(maxNum, tmp.cnt + 1);
+            }
+//            System.out.println("p2 : " +tmp);
+
+        }
+        map[p2.r][p2.c] = 1;
+//        viewTest(p1, p2, minNum, maxNum, dep+1);
+        return new ResultInfo(playerStatus, playerStatus ? minNum : maxNum);
+    }
 
     static boolean chkOut(int r, int c) {
         return r < 0 || r >= R || c < 0 || c >= C || map[r][c] == 0;
@@ -114,27 +116,34 @@ public class Solution2 {
         }
     }
 
-    static class ResultInfo implements Comparable<ResultInfo> {
-        int winner; // a가 0, b가 1
+    static class ResultInfo {
+        boolean winner; // a가 0, b가 1
         int cnt;
 
-        public ResultInfo(int winner, int cnt) {
+        public ResultInfo(boolean winner, int cnt) {
             this.winner = winner;
             this.cnt = cnt;
-
-        }
-
-
-        @Override
-        public int compareTo(ResultInfo o) {
-//            return Integer.compare(this.winLog.length() + this.loseLog.length(), o.winLog.length() + o.loseLog.length());
-//            return this.winLog.length() != o.winLog.length() ? Integer.compare(this.winLog.length(), o.winLog.length()) : Integer.compare(this.loseLog.length(), o.loseLog.length());
-            return this.winner != o.winner ? Integer.compare(this.winner, o.winner) : Integer.compare(this.cnt, o.cnt);
         }
 
         public String toString() {
             return winner + "/" + cnt;
         }
+    }
 
+    static void viewTest(Player p1, Player p2, int min, int max, int dep) {
+        System.out.println();
+        System.out.println(dep + ":" + min + "," + max);
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (p1.r == r && p1.c == c) {
+                    System.out.print("A ");
+                } else if (p2.r == r && p2.c == c) {
+                    System.out.print("B ");
+                } else {
+                    System.out.print(map[r][c] + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
